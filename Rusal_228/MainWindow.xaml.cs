24e03c63b45b8789d4e2cf8b208e0d5de3290d98
LuckyTwice <mkhaidarov63@gmail.com>
2023-11-05 13:58:56 +0700

    Добавил основную форму и докачал необходимые пакеты

--- Rusal_228/MainWindow.xaml.cs
+++ Rusal_228/MainWindow.xaml.cs
@@ -24,5 +24,117 @@ namespace Rusal_228
         {
             InitializeComponent();
         }
+        /*private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private OdbcConnection connection;
+        private void Done_Click(object sender, RoutedEventArgs e)
+        {
+            string username = Login.Text;
+            string password = Password.Password;
+
+            if (VerifyLogin(username, password))
+            {
+                MessageBox.Show("11");
+                int proff = GetProffFromTable2(username);
+                if (VerifyProff(proff) == "Работник")
+                {
+                    OpenWindow1();
+                }
+                else if (VerifyProff(proff) == "Админ")
+                {
+                    OpenWindow2();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.");
+            }
+        }
+        private bool VerifyLogin(string username, string password)
+        {
+            string connectionString = "Dsn=Rusal;data source=localhost;database=Rusal;user id=postgres;schema=Rusal_Schema"; // Replace with your actual connection string
+
+            using (connection = new OdbcConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = $"SELECT COUNT(*) FROM \"Rusal_Shema\".\"Password\" WHERE \"Id\" = '{username}' AND \"Password\" = '{password}'";
+                    using (OdbcCommand command = new OdbcCommand(query, connection))
+                    {
+                        int count = Convert.ToInt32(command.ExecuteScalar());
+                        return count > 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception
+                    MessageBox.Show("An error occurred while connecting to the database.");
+                    return false;
+                }
+            }
+        }
+        private int GetProffFromTable2(string username)
+        {
+            string connectionString = "Dsn=Rusal;data source=localhost;database=Rusal;user id=postgres;schema=Rusal_Schema"; // Replace with your actual connection string
+
+            using (connection = new OdbcConnection(connectionString))
+            {
+                connection.Open();
+                string query = $"SELECT \"Профессия\" FROM \"Rusal_Shema\".\"Personal\" WHERE \"Id\" = '{username}'";
+
+                using (OdbcCommand command = new OdbcCommand(query, connection))
+                {
+                    *//* try
+                     {*//*
+                    return Convert.ToInt32(command.ExecuteScalar());
+                    *//* }*/
+                    /* catch (Exception ex)
+                     {
+                         // Handle the exception
+                         MessageBox.Show("An error occurred while retrieving Proff from Table2.");
+                         return -1;
+                     }*//*
+                }
+            }
+        }
+
+        private string VerifyProff(int proff)
+        {
+            string connectionString = "Dsn=Rusal;data source=localhost;database=Rusal;user id=postgres;schema=Rusal_Schema"; // Replace with your actual connection string
+
+            using (connection = new OdbcConnection(connectionString))
+            {
+                connection.Open();
+                string query = $"SELECT \"Профессия\" FROM \"Rusal_Shema\".\"Professia\" WHERE \"Id\" = '{proff}'";
+
+                using (OdbcCommand command = new OdbcCommand(query, connection))
+                {
+                    *//*                try
+                                    {*//*
+                    return command.ExecuteScalar()?.ToString();
+                    *//* }
+                     catch (Exception ex)
+                     {
+                         // Handle the exception
+                         MessageBox.Show("An error occurred while verifying Proff in Table3.");
+                         return null;
+                     }*//*
+                }
+            }
+        }
+        private void OpenWindow1()
+        {
+            WorkerMain worker = new WorkerMain();
+            worker.ShowDialog();
+        }
+        private void OpenWindow2()
+        {
+            AdminMain admin = new AdminMain();
+            admin.ShowDialog();
+        }*/
     }
 }
