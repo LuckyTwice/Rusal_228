3034b8b3d50c880b2c9e33f59e490d581d2b95de
LuckyTwice <mkhaidarov63@gmail.com>
2023-11-05 19:32:57 +0700

    Сделал перенос имени в TextBox на другую форму

--- Rusal_228/MainWindow.xaml.cs
+++ Rusal_228/MainWindow.xaml.cs
@@ -31,14 +31,8 @@ namespace Rusal_228
         
         private void Done_Click(object sender, RoutedEventArgs e)
         {
-            using (AdminContext db  = new AdminContext()) 
+            /*using (AdminContext db  = new AdminContext()) 
             {
-                /*var users = db.Personals.ToList();
-                ///Console.WriteLine("список");
-                foreach (var user in users) 
-                {
-                    Console.WriteLine($"{user.Id}.{user.Имя}.{user.Фамилия} - {user.Профессия}");
-                }*/
                 var phones = db.Personals.ToList();
                 string message = "";
                 foreach(var person in phones) 
@@ -46,33 +40,36 @@ namespace Rusal_228
                     message += $"Имя:{person.Имя}, Фамилия:{person.Фамилия}{Environment.NewLine}";
                 }
                 MessageBox.Show(message,"Список");
-            }
+            }*/
+            
             int username = Convert.ToInt32(Login.Text);
-            string password = Password.Password;
+            int password = Convert.ToInt32(Password.Password);
 
-            /*if (VerifyLogin(username, password))
+            if (VerifyLogin(username, password))
             {
                 MessageBox.Show("11");
                 int proff = GetProffFromTable2(username);
                 if (VerifyProff(proff) == "Работник")
                 {
-                    OpenWindow1();
+                    OpenWindow1(username);
                 }
                 else if (VerifyProff(proff) == "Админ")
                 {
-                    OpenWindow2();
+                    OpenWindow2(username);
                 }
             }
             else
             {
                 MessageBox.Show("Invalid username or password.");
-            }*/
+            }
         }
-        private bool VerifyLogin(int username, string password)
+        private bool VerifyLogin(int username, int password)
         {
             using(AdminContext db = new AdminContext()) 
             {
-                var count = db.Passwords.
+                int verify = db.Passwords.Count(p => p.Id == username && p.Password1 == password);
+                return verify > 0;
+            }
             /*using (connection = new OdbcConnection(connectionString))
             {
                 try
@@ -94,64 +91,77 @@ namespace Rusal_228
                 }
             }*/
         }
-        /*private int GetProffFromTable2(string username)
+        private int GetProffFromTable2(int username)
         {
-            string connectionString = "Dsn=Rusal;data source=localhost;database=Rusal;user id=postgres;schema=Rusal_Schema"; // Replace with your actual connection string
-
-            using (connection = new OdbcConnection(connectionString))
+            using (AdminContext db = new AdminContext())
+            {
+                //Возможно использование First это костыль
+                var id = db.Personals.Where(p => p.Id == username).Select(p=> p.Профессия ).First();
+                return Convert.ToInt32(id);
+            }
+            /*using (connection = new OdbcConnection(connectionString))
             {
                 connection.Open();
                 string query = $"SELECT \"Профессия\" FROM \"Rusal_Shema\".\"Personal\" WHERE \"Id\" = '{username}'";
 
                 using (OdbcCommand command = new OdbcCommand(query, connection))
                 {
-                    *//* try
-                     {*//*
-                    return Convert.ToInt32(command.ExecuteScalar());
-                    *//* }*/
-                    /* catch (Exception ex)
-                     {
-                         // Handle the exception
-                         MessageBox.Show("An error occurred while retrieving Proff from Table2.");
-                         return -1;
-                     }*//*
+                    try
+                    {
+                        return Convert.ToInt32(command.ExecuteScalar());
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle the exception
+                        MessageBox.Show("An error occurred while retrieving Proff from Table2.");
+                        return -1;
+                    }
                 }
-            }
-        }*//*
+            }*/
+        }
 
         private string VerifyProff(int proff)
         {
-            string connectionString = "Dsn=Rusal;data source=localhost;database=Rusal;user id=postgres;schema=Rusal_Schema"; // Replace with your actual connection string
-
-            using (connection = new OdbcConnection(connectionString))
+            using (AdminContext db = new AdminContext())
+            {
+                var prof = db.Professia.Where(p => p.Id == proff).Select(p => p.Професия).First();
+                return prof.ToString();
+            }
+            /*using (connection = new OdbcConnection(connectionString))
             {
                 connection.Open();
                 string query = $"SELECT \"Профессия\" FROM \"Rusal_Shema\".\"Professia\" WHERE \"Id\" = '{proff}'";
 
                 using (OdbcCommand command = new OdbcCommand(query, connection))
                 {
-                    *//*                try
-                                    {*//*
-                    return command.ExecuteScalar()?.ToString();
-                    *//* }
-                     catch (Exception ex)
-                     {
-                         // Handle the exception
-                         MessageBox.Show("An error occurred while verifying Proff in Table3.");
-                         return null;
-                     }*//*
+                    try
+                    {
+                        return command.ExecuteScalar()?.ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle the exception
+                        MessageBox.Show("An error occurred while verifying Proff in Table3.");
+                        return null;
+                    }
                 }
-            }
+            }*/
         }
-        private void OpenWindow1()
+        private void OpenWindow1(int id)
         {
+            AdminContext db = new AdminContext();
+            var fio = db.Personals.Where(p => p.Id == id).Select(p => new { p.Фамилия, p.Имя, p.Отчество }).First();
             WorkerMain worker = new WorkerMain();
+            worker.Name.Text = $"{fio.Фамилия} {fio.Имя} {fio.Отчество}";
             worker.ShowDialog();
         }
-        private void OpenWindow2()
+        private void OpenWindow2(int id)
         {
+            AdminContext db = new AdminContext();
+            var fio = db.Personals.Where(p => p.Id == id).Select(p => new {p.Фамилия, p.Имя, p.Отчество }).First();
             AdminMain admin = new AdminMain();
+            admin.Name.Text = $"{fio.Фамилия} {fio.Имя} {fio.Отчество}";
             admin.ShowDialog();
-        }*/
-                }
-            }
+        }
+    }
+}
