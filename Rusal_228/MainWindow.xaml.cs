a2195f0e51cb0231d72f00cd4439947bf5776d7f
LuckyTwice <mkhaidarov63@gmail.com>
2023-11-15 16:11:40 +0700

    Добавил проверку на вход и немного оптимизировал код

--- Rusal_228/MainWindow.xaml.cs
+++ Rusal_228/MainWindow.xaml.cs
@@ -31,133 +31,71 @@ namespace Rusal_228
         
         private void Done_Click(object sender, RoutedEventArgs e)
         {
-            /*using (AdminContext db  = new AdminContext()) 
+            using (var db = new AdminContext())
             {
-                var phones = db.Personals.ToList();
-                string message = "";
-                foreach(var person in phones) 
+                bool isConnected = db.Database.CanConnect();
+                if (isConnected)
                 {
-                    message += $"Имя:{person.Имя}, Фамилия:{person.Фамилия}{Environment.NewLine}";
-                }
-                MessageBox.Show(message,"Список");
-            }*/
-            
-            int username = Convert.ToInt32(Login.Text);
-            int password = Convert.ToInt32(Password.Password);
-
-            if (VerifyLogin(username, password))
-            {
-                MessageBox.Show("11");
-                int proff = GetProffFromTable2(username);
-                if (VerifyProff(proff) == "Работник")
-                {
-                    OpenWindow1(username);
-                }
-                else if (VerifyProff(proff) == "Админ")
-                {
-                    OpenWindow2(username);
-                }
-            }
-            else
-            {
-                MessageBox.Show("Invalid username or password.");
-            }
-        }
-        private bool VerifyLogin(int username, int password)
-        {
-            using(AdminContext db = new AdminContext()) 
-            {
-                int verify = db.Passwords.Count(p => p.Id == username && p.Password1 == password);
-                return verify > 0;
-            }
-            /*using (connection = new OdbcConnection(connectionString))
-            {
-                try
-                {
-                    connection.Open();
-
-                    string query = $"SELECT COUNT(*) FROM \"Rusal_Shema\".\"Password\" WHERE \"Id\" = '{username}' AND \"Password\" = '{password}'";
-                    using (OdbcCommand command = new OdbcCommand(query, connection))
+                    try
                     {
-                        int count = Convert.ToInt32(command.ExecuteScalar());
-                        return count > 0;
+                        int username = Convert.ToInt32(Login.Text);
+                        int password = Convert.ToInt32(Password.Password);
+                        if (VerifyLogin(db, username, password))
+                        {
+                            MessageBox.Show("11");
+                            int proff = GetProffFromTable2(db, username);
+                            if (VerifyProff(db, proff) == "Работник")
+                            {
+                                OpenWindow1(db, username);
+                            }
+                            else if (VerifyProff(db, proff) == "Админ")
+                            {
+                                OpenWindow2(db, username);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Неверно введены логин или пароль");
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        MessageBox.Show("Логин и пароль дожны быть введены цифрами");
                     }
                 }
-                catch (Exception ex)
+                else
                 {
-                    // Handle the exception
-                    MessageBox.Show("An error occurred while connecting to the database.");
-                    return false;
+                    MessageBox.Show("Отсутствует связь с сервером", "Ошибка");
                 }
-            }*/
+            }
+           
         }
-        private int GetProffFromTable2(int username)
+        private bool VerifyLogin(AdminContext db, int username, int password)
         {
-            using (AdminContext db = new AdminContext())
-            {
+                int verify = db.Passwords.Count(p => p.Id == username && p.Password1 == password);
+                return verify > 0;
+        }
+        private int GetProffFromTable2(AdminContext db, int username)
+        {
                 //Возможно использование First это костыль
                 var id = db.Personals.Where(p => p.Id == username).Select(p=> p.Профессия ).First();
                 return Convert.ToInt32(id);
-            }
-            /*using (connection = new OdbcConnection(connectionString))
-            {
-                connection.Open();
-                string query = $"SELECT \"Профессия\" FROM \"Rusal_Shema\".\"Personal\" WHERE \"Id\" = '{username}'";
-
-                using (OdbcCommand command = new OdbcCommand(query, connection))
-                {
-                    try
-                    {
-                        return Convert.ToInt32(command.ExecuteScalar());
-                    }
-                    catch (Exception ex)
-                    {
-                        // Handle the exception
-                        MessageBox.Show("An error occurred while retrieving Proff from Table2.");
-                        return -1;
-                    }
-                }
-            }*/
         }
 
-        private string VerifyProff(int proff)
+        private string VerifyProff(AdminContext db, int proff)
         {
-            using (AdminContext db = new AdminContext())
-            {
                 var prof = db.Professia.Where(p => p.Id == proff).Select(p => p.Професия).First();
                 return prof.ToString();
-            }
-            /*using (connection = new OdbcConnection(connectionString))
-            {
-                connection.Open();
-                string query = $"SELECT \"Профессия\" FROM \"Rusal_Shema\".\"Professia\" WHERE \"Id\" = '{proff}'";
-
-                using (OdbcCommand command = new OdbcCommand(query, connection))
-                {
-                    try
-                    {
-                        return command.ExecuteScalar()?.ToString();
-                    }
-                    catch (Exception ex)
-                    {
-                        // Handle the exception
-                        MessageBox.Show("An error occurred while verifying Proff in Table3.");
-                        return null;
-                    }
-                }
-            }*/
         }
-        private void OpenWindow1(int id)
+        private void OpenWindow1(AdminContext db, int id)
         {
-            AdminContext db = new AdminContext();
             var fio = db.Personals.Where(p => p.Id == id).Select(p => new { p.Фамилия, p.Имя, p.Отчество }).First();
             WorkerMain worker = new WorkerMain();
             worker.Name.Text = $"{fio.Фамилия} {fio.Имя} {fio.Отчество}";
             worker.ShowDialog();
         }
-        private void OpenWindow2(int id)
+        private void OpenWindow2(AdminContext db, int id)
         {
-            AdminContext db = new AdminContext();
             var fio = db.Personals.Where(p => p.Id == id).Select(p => new {p.Фамилия, p.Имя, p.Отчество }).First();
             AdminMain admin = new AdminMain();
             admin.Name.Text = $"{fio.Фамилия} {fio.Имя} {fio.Отчество}";
